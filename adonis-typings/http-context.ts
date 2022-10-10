6e0cf23c5a8585ef35864a89ebea2e719a489141
Fiacre Dossou AYEDOUN <fayedoun@sfp-groupe.com>
2022-10-10 15:24:30 +0100

    hotfix: clear error

--- adonis-typings/http-context.ts
+++ adonis-typings/http-context.ts
@@ -1,5 +1,7 @@
 declare module "@ioc:Adonis/Core/HttpContext" {
+  import {SourceContract} from "@ioc:Adonis/Addons/ServerSentEvent";
+
   interface HttpServerSentEventContextContract extends HttpContextContract {
-    source: any;
+    source: SourceContract;
   }
 }
